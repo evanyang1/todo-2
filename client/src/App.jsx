9a33f1ab9db23c4969a3f1e3733438b612470f05
Evan Yang <evanjyang4@gmail.com>
2025-09-14 10:09:35 -0700

    tanstack, tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 
-import './App.css'
+import "./App.css";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+const __TSR_ROUTER_TYPE_GUARD__ = router;
 
 function App() {
-
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
