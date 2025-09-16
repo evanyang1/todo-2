3b35467d51c160017ad1b84fd8060fdf0e58922b
Evan Yang <evanjyang4@gmail.com>
2025-09-16 00:16:54 -0700

    login, register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,9 +8,59 @@ const express = require("express");
 
 const userRouter = express.Router();
 
-userRouter.post("/register", async (req, res) => {});
+userRouter.post("/register", async (req, res) => {
+  const { email, password } = req.body;
 
-userRouter.post("/login", async (req, res) => {});
+  try {
+    const userExists = await userModel.findOne({ email });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
 
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await userModel({
+      email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+    const token = createToken(user._id);
+    res.status(201).json({ success: true, token });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = createToken(user._id);
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 module.exports = userRouter;
