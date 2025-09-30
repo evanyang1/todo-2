af2794a0ce3947f6a0d325d420cab75b4e2b4cdb
Evan Yang <evanjyang4@gmail.com>
2025-10-01 00:06:36 +0800

    no duplicates

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,26 +8,7 @@ const validator = require("validator");
 const express = require("express");
 
 const userRouter = express.Router();
-
-const authMiddleware = async (req, res, next) => {
-  console.log("authMiddleware");
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Authorization token required" });
-  }
-
-  const token = authorization.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // Attach user to the request object, excluding the password
-    req.user = await userModel.findById(decoded.id).select("-password");
-    next();
-  } catch (error) {
-    console.error("Authentication error:", error);
-    res.status(401).json({ message: "Request is not authorized" });
-  }
-};
+const authMiddleware = require("../middleware/authMiddleware");
 
 userRouter.post("/register", async (req, res) => {
   const { email, password } = req.body;
