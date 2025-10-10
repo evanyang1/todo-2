23b9ecc8fd0a02728bde8919a24fd8d3df200734
Evan Yang <evanjyang4@gmail.com>
2025-10-10 12:05:44 +0800

    background color changes when the due date is past

--- client/src/components/TasksList.jsx
+++ client/src/components/TasksList.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import useTaskStore from "../store/taskStore";
 
+const determineBackgroundColor = (date) => {
+  const today = new Date();
+  // if date is before today, return red
+  if (new Date(date) < today) {
+    return "bg-red-100";
+  } else {
+    return "bg-white";
+  }
+};
+
 const TasksList = () => {
   return (
     <div>
       {useTaskStore((state) => state.tasks).map((task) => (
-        <div key={task._id} className="p-4 mb-4 bg-white rounded-lg shadow">
+        <div
+          key={task._id}
+          className={`p-4 mb-4 ${determineBackgroundColor(task.dueDate)} rounded-lg shadow`}
+        >
           <h2 className="text-xl font-semibold text-gray-800">{task.name}</h2>
           <p className="text-gray-600">
             Due: {new Date(task.dueDate).toLocaleDateString()}
